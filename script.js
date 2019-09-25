dae686baf975294ff3add17bcc95d0594894cca9
Lindsey Brown <libersword@gmail.com>
2019-09-25 16:45:30 -0600

    Wrote the basic logic for the password gen to run

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      
+//Declare Variables
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789'
+var specialChar = '0123456789';
+var characters = '';
+var displayPassword = document.getElementById("genPassword");
 
-      charlength = characters.length;
-      
-      function randomChar (letters){
-        var password ='';
-        for (var i =0; i<letters; i++){
-          password+=characters.charAt(Math.floor(Math.random()*characters.length));
-        }
+//Prompt the user for password generation
+var userLength = prompt("How long do you want your password to be? Must be between 8 and 128 characters.");
+Number(userLength);
+var userSpecial = prompt("Do you want special characters?");
+var userNumber = prompt("Do you want numbers?");
+var userLower = prompt("Do you want uppercase letters?");
+var userUpper = prompt("Do you want lowercase letters?");
 
-        return password; 
-      }
-      
+//If the user wants the various requirements, concatonate want they want into one string of characters for the program to choose from
+if (userUpper === "yes") {
+  characters += uppercase;
+};
+if (userLower === "yes") {
+  characters += lowercase;
+};
+if (userNumber === "yes") {
+  characters += numbers;
+};
+if (userSpecial === "yes") {
+  characters += specialChar;
+};
+
+//The function to generate the password
+function passwordGenerator(length, characters) {
+  var password = ' ';
+  for (var i=0; i<length; i++)
+  {
+    password +=characters.charAt(Math.floor(Math.random()*characters.length));
+  }
+  return password;
+}
+
+//for testing purposes
+console.log(passwordGenerator(userLength, characters));
+
+//displaying the password
+displayPassword.textContent = 'Your secure password is: ' + passwordGenerator(userLength, characters);
